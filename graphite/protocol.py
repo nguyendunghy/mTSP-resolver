ec598c315ac5c803ae8fcab07a1d5622c3ca6e32
dungnguyen <dung.kstncnttk56@gmail.com>
2024-09-13 00:18:11 +0700

    don't change protocol.py

--- graphite/protocol.py
+++ graphite/protocol.py
@@ -27,13 +27,6 @@ import json
 import base64
 import sys
 import random
-from graphite.data.constants import ASIA_MSB_DETAILS, WORLD_TSP_DETAILS
-from graphite.data.dataset_utils import load_dataset
-
-loaded_datasets = {
-    ASIA_MSB_DETAILS['ref_id']: load_dataset(ASIA_MSB_DETAILS['ref_id']),
-    WORLD_TSP_DETAILS['ref_id']: load_dataset(WORLD_TSP_DETAILS['ref_id'])
-}
 
 is_alive_path = "graphite/is_alive.json"
 with open(is_alive_path, "r") as f:
@@ -151,35 +144,6 @@ class GraphV2Problem(BaseModel):
     repeating: bool = Field(False, description="Allow Repeating Nodes")  # boolean for whether the nodes in the problem can be revisited
     checksum: Union[str, None] = Field(None, description="Checksum")
 
-    @model_validator(mode='after') # checks and generates missing data if not passed in by user
-    def initialize_nodes_and_edges(self):
-        if self.directed == False: # we should generate a set of coordinates and corresponding distances for this Metric TSP
-            if not self.nodes and not self.edges: # we do not want to allow setting of edges for metric TSP... only setting coordinates
-                datas = np.array(loaded_datasets[self.dataset_ref]['data'])
-                self.nodes = [[datas[idx - 1][1], datas[idx - 1][2]] for idx in self.selected_ids]
-                self.edges = self.get_distance_matrix(self.nodes)
-            elif self.edges and not self.nodes: # convert problem into a General TSP and solve instead
-                raise ValueError("Undirected graph should have defined nodes(coordinates) instead of edges")
-            elif not self.edges and self.nodes:
-                self.edges = self.get_distance_matrix(self.nodes)
-        return self
-
-    def get_distance_matrix(self, coordinates:List[List[int]]):
-        '''
-        Receives an array of the coordinate values and returns a square numpy array of euclidean distances
-        '''
-        n = len(coordinates)
-        distance_matrix = np.zeros((n, n))
-
-        for i in range(n):
-            for j in range(n):
-                if i == j:
-                    distance_matrix[i, j] = 0
-                else:
-                    distance_matrix[i, j] += math.hypot((coordinates[i][0] - coordinates[j][0]), (coordinates[i][1] - coordinates[j][1]))
-        distance_list = distance_matrix.tolist()
-        distance_list = [[float(x) for x in row] for row in distance_list]
-        return distance_list
     ### Expensive check only needed for organic requests
     # @model_validator(mode='after')
     # def unique_select_ids(self):
@@ -340,6 +304,143 @@ class GraphV1Problem(BaseModel):
     def validate_length(self):
         if not self.directed and len(self.nodes) != self.n_nodes:
             raise ValueError(f"The length of nodes {len(self.nodes)} does not match n_nodes {self.n_nodes}")
+
+        if not self.directed and not all(len(row)==2 for row in self.nodes):
+            raise ValueError(f"the input coordinates are not all 2-dimensional")
+
+        if len(self.edges) != self.n_nodes:
+            raise ValueError(f"The length of edges {len(self.edges)} does not match n_nodes {self.n_nodes}")
+
+        if not all(len(row)==self.n_nodes for row in self.edges):
+            raise ValueError(f"The length of edges {len(self.edges)} does not match n_nodes {self.n_nodes}")
+        return self
+
+    @model_validator(mode='after')
+    def validate_values(self):
+        if self.directed:
+            # check edges; Non-diagonal must be valid number
+            for i in range(self.n_nodes):
+                for j in range(self.n_nodes):
+                    if i != j:
+                        assert isinstance(self.edges[i][j], (float, int)), TypeError(f"Found value in edges at index {(i,j)} of type {type(self.edges[i][j])}")
+                        assert (math.isfinite(self.edges[i][j]) and np.isfinite(self.edges[i][j])), ValueError(f"Found non-finite value in edges at index {(i,j)} of value {self.edges[i][j]}")
+        else:
+            # check nodes
+            for i, node in enumerate(self.nodes):
+                for j, coordinate_val in enumerate(node):
+                    assert isinstance(coordinate_val, (float, int)), TypeError(f"Found value in nodes at index {(i,j)} of type {type(coordinate_val)}")
+                    assert (math.isfinite(self.edges[i][j]) and np.isfinite(self.edges[i][j])), ValueError(f"Found non-finite value in nodes at index {(i,j)} of value {coordinate_val}")
+        return self
+
+    @model_validator(mode='after')
+    def force_obj_function(self):
+        if self.problem_type in ['Metric TSP', 'General TSP']:
+            assert self.objective_function == 'min', ValueError('Subnet currently only supports minimization TSP')
+        return self
+
+    def generate_random_coordinates(self, n_cities, grid_size=1000):
+        # Create a grid of coordinates
+        x = np.arange(grid_size)
+        y = np.arange(grid_size)
+        xv, yv = np.meshgrid(x, y)
+
+        coordinates = np.column_stack((xv.ravel(), yv.ravel()))
+
+        # Sample n_cities coordinates from the grid
+        sampled_indices = np.random.choice(coordinates.shape[0], n_cities, replace=False)
+        sampled_coordinates = coordinates[sampled_indices]
+        np.random.shuffle(sampled_coordinates)
+
+        # Convert the array to a list of lists of integers
+        sampled_coordinates_list = sampled_coordinates.tolist()
+        sampled_coordinates_list = [[int(coord) for coord in pair] for pair in sampled_coordinates_list]
+
+        return sampled_coordinates_list
+
+    def get_distance_matrix(self, coordinates:List[List[int]]):
+        '''
+        Receives an array of the coordinate values and returns a square numpy array of euclidean distances
+        '''
+        n = len(coordinates)
+        distance_matrix = np.zeros((n, n))
+
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    distance_matrix[i, j] = 0
+                else:
+                    distance_matrix[i, j] = np.sqrt((coordinates[i][0] - coordinates[j][0])**2 + (coordinates[i][1] - coordinates[j][1])**2)
+        distance_list = distance_matrix.tolist()
+        distance_list = [[float(x) for x in row] for row in distance_list]
+        return distance_list
+
+    @staticmethod
+    def generate_edges(num_cities, min_weight=1, max_weight=100):
+        # Initialize a matrix with zeros
+        edges = np.zeros((num_cities, num_cities), dtype=int)
+        for i in range(num_cities):
+            for j in range(0, num_cities):
+                weight = np.random.randint(min_weight, max_weight)
+                edges[i][j] = weight
+        edges_list = edges.tolist()
+        edges_list = [[int(cost) for cost in row] for row in edges_list]
+        return edges_list
+
+    def get_info(self, verbosity: int = 1) -> dict:
+        info = {}
+        if verbosity == 1:
+            info["Problem Type"] = self.problem_type
+        elif verbosity == 2:
+            info["Problem Type"] = self.problem_type
+            info["Objective Function"] = self.objective_function
+            info["To Visit All Nodes"] = self.visit_all
+            info["To Return to Origin"] = self.to_origin
+            info["Number of Nodes"] = self.n_nodes
+            info["Directed"] = self.directed
+            info["Simple"] = self.simple
+            info["Weighted"] = self.weighted
+            info["Repeating"] = self.repeating
+        elif verbosity == 3:
+            for field in self.model_fields:
+                description = self.model_fields[field].description
+                value = getattr(self, field)
+                info[description] = value
+        return info
+
+
+class GraphProblem(BaseModel):
+    problem_type: Literal['Metric TSP', 'General TSP'] = Field('Metric TSP', description="Problem Type")
+    objective_function: str = Field('min', description="Objective Function")
+    visit_all: bool = Field(True, description="Visit All Nodes")
+    to_origin: bool = Field(True, description="Return to Origin")
+    n_nodes: conint(ge=2) = Field(10, description="Number of Nodes (must be >= 2)")
+    nodes: Union[List[List[Union[conint(ge=0), confloat(ge=0)]]],None] = Field(default_factory=list, description="Node Coordinates")  # If not none, nodes represent the coordinates of the cities
+    edges: Union[List[List[Union[conint(ge=0), confloat(ge=0)]]],None] = Field(default_factory=list, description="Edge Weights")  # If not none, this represents a square matrix of edges where edges[source;row][destination;col] is the cost of a given edge
+    directed: bool = Field(False, description="Directed Graph")  # boolean for whether the graph is directed or undirected / Symmetric or Asymmetric
+    simple: bool = Field(True, description="Simple Graph")  # boolean for whether the graph contains any degenerate loop
+    weighted: bool = Field(False, description="Weighted Graph")  # boolean for whether the value in the edges matrix represents cost
+    repeating: bool = Field(False, description="Allow Repeating Nodes")  # boolean for whether the nodes in the problem can be revisited
+
+    @model_validator(mode='after') # checks and generates missing data if not passed in by user
+    def initialize_nodes_and_edges(self):
+        if self.directed == False: # we should generate a set of coordinates and corresponding distances for this Metric TSP
+            if not self.nodes and not self.edges: # we do not want to allow setting of edges for metric TSP... only setting coordinates
+                self.nodes = self.generate_random_coordinates(self.n_nodes)
+                self.edges = self.get_distance_matrix(self.nodes)
+            elif self.edges and not self.nodes: # convert problem into a General TSP and solve instead
+                raise ValueError("Undirected graph should have defined nodes(coordinates) instead of edges")
+            elif not self.edges and self.nodes:
+                self.edges = self.get_distance_matrix(self.nodes)
+        else: # we need to generate a set of random edge weights for this General TSP
+            self.problem_type = 'General TSP'
+            if not self.edges:
+                self.edges = self.generate_edges(self.n_nodes)
+        return self
+
+    @model_validator(mode='after')
+    def validate_length(self):
+        if not self.directed and len(self.nodes) != self.n_nodes:
+            raise ValueError(f"The length of nodes {len(self.nodes)} does not match n_nodes {self.n_nodes}")
         
         if not self.directed and not all(len(row)==2 for row in self.nodes):
             raise ValueError(f"the input coordinates are not all 2-dimensional")
@@ -448,6 +549,13 @@ class GraphV1Synapse(bt.Synapse):
     Implement necessary serialization and deserialization checks
     '''
     problem: GraphV1Problem
+    solution: Optional[Union[List[int], bool]] = None
+
+class GraphSynapse(bt.Synapse):
+    '''
+    Implement necessary serialization and deserialization checks
+    '''
+    problem: GraphProblem
     solution: Optional[Union[List[int], bool]] = None
 
     def to_headers(self) -> dict:
