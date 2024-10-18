b9b4e55ce1ab4239ad0c97eeeabe13e4b62dd14e
dungnguyen <dung.kstncnttk56@gmail.com>
2024-10-18 12:16:16 +0700

    add max_node

--- neurons/lkh_app.py
+++ neurons/lkh_app.py
@@ -14,6 +14,7 @@ def parse():
     parser.add_argument('--port', type=int, default=8080, help='Port to run the server on.')
     parser.add_argument('--num_run', type=str, default='', help='Number of running')
     parser.add_argument('--max_trial', type=str, default='', help='Maximum of trial')
+    parser.add_argument('--max_node', type=str, default=5000, help='Maximum of trial')
     return parser.parse_args()
 
 
@@ -41,6 +42,8 @@ def lkh_resolve():
         n_nodes = problem['n_nodes']
         dataset_ref = problem['dataset_ref']
         timeout = problem['timeout']
+        if n_nodes > args.max_node:
+            return jsonify({"message": f"PROBLEM TOO LARGE", "result": None, "score": 0}), 200
 
         lkh_solver = LKHSolver(num_run=args.num_run, max_trial=args.max_trial, input_file=input_file)
         graph_problem = GraphV2Problem(problem_type="Metric TSP", n_nodes=n_nodes, selected_ids=[0],
