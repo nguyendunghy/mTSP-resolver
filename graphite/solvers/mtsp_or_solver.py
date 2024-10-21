36517198f311500dd4331ef0fb4865dae7b6e1f1
dungnguyen <dung.kstncnttk56@gmail.com>
2024-10-22 02:21:24 +0700

    test mTSP or-solver

--- graphite/solvers/mtsp_or_solver.py
+++ graphite/solvers/mtsp_or_solver.py
@@ -1,3 +1,4 @@
+import traceback
 from typing import List
 from typing import Union
 
@@ -12,53 +13,57 @@ class MTSP_ORToolsSolver(BaseSolver):
         super().__init__(problem_types=problem_types)
 
     async def solve(self, problem, future_id: int) -> List[int]:
-        print(f'start solve. problem in mtsp_or_solver')
-        data = self.create_data_model(distance_matrix=problem.edges,num_vehicles=problem.n_salesmen,depot=0)
-        manager = pywrapcp.RoutingIndexManager(
-            len(data["distance_matrix"]), data["num_vehicles"], data["depot"]
-        )
-        routing = pywrapcp.RoutingModel(manager)
+        try:
 
-        # Create and register a transit callback.
-        def distance_callback(from_index, to_index):
-            """Returns the distance between the two nodes."""
-            # Convert from routing variable Index to distance matrix NodeIndex.
-            from_node = manager.IndexToNode(from_index)
-            to_node = manager.IndexToNode(to_index)
-            return data["distance_matrix"][from_node][to_node]
+            print(f'start solve. problem in mtsp_or_solver')
+            data = self.create_data_model(distance_matrix=problem.edges,num_vehicles=problem.n_salesmen,depot=0)
+            manager = pywrapcp.RoutingIndexManager(
+                len(data["distance_matrix"]), data["num_vehicles"], data["depot"]
+            )
+            routing = pywrapcp.RoutingModel(manager)
 
-        transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+            # Create and register a transit callback.
+            def distance_callback(from_index, to_index):
+                """Returns the distance between the two nodes."""
+                # Convert from routing variable Index to distance matrix NodeIndex.
+                from_node = manager.IndexToNode(from_index)
+                to_node = manager.IndexToNode(to_index)
+                return data["distance_matrix"][from_node][to_node]
 
-        # Define cost of each arc.
-        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+            transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 
-        # Add Distance constraint.
-        dimension_name = "Distance"
-        routing.AddDimension(
-            transit_callback_index,
-            0,  # no slack
-            3000,  # vehicle maximum travel distance
-            True,  # start cumul to zero
-            dimension_name,
-        )
+            # Define cost of each arc.
+            routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-        distance_dimension = routing.GetDimensionOrDie(dimension_name)
-        distance_dimension.SetGlobalSpanCostCoefficient(100)
-        # Setting first solution heuristic.
-        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-        search_parameters.first_solution_strategy = (
-            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
-        )
-        # Solve the problem.
-        solution = routing.SolveWithParameters(search_parameters)
+            # Add Distance constraint.
+            dimension_name = "Distance"
+            routing.AddDimension(
+                transit_callback_index,
+                0,  # no slack
+                3000,  # vehicle maximum travel distance
+                True,  # start cumul to zero
+                dimension_name,
+            )
 
-        # Print solution on console.
-        if solution:
-            self.print_solution(data, manager, routing, solution)
-        else:
-            print("No solution found !")
+            distance_dimension = routing.GetDimensionOrDie(dimension_name)
+            distance_dimension.SetGlobalSpanCostCoefficient(100)
+            # Setting first solution heuristic.
+            search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+            search_parameters.first_solution_strategy = (
+                routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+            )
+            # Solve the problem.
+            solution = routing.SolveWithParameters(search_parameters)
 
+            # Print solution on console.
+            if solution:
+                self.print_solution(data, manager, routing, solution)
+            else:
+                print("No solution found !")
 
+        except Exception as e:
+            print(f'Exception {e}')
+            traceback.print_exc()
         return []
 
     def problem_transformations(self, problem: Union[GraphV2ProblemMulti]):
