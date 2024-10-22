c4f343fb4aeb41bf228ab990fa8be17403287bc0
dungnguyen <dung.kstncnttk56@gmail.com>
2024-10-22 15:28:06 +0700

    test nn-multi solver

--- neurons/compare_solutions.py
+++ neurons/compare_solutions.py
@@ -21,6 +21,7 @@ loaded_datasets = {
     WORLD_TSP_DETAILS['ref_id']: load_dataset(WORLD_TSP_DETAILS['ref_id'])
 }
 
+
 def generate_problem():
     prob_select = random.randint(1, 2)
 
@@ -32,7 +33,7 @@ def generate_problem():
             problems, sizes = GeneralTSPGenerator.generate_n_samples(1)
             test_problem_obj = problems[0]
     except ValidationError as e:
-        bt.logging.debug(f"{'Metric TSP' if prob_select==1 else 'General TSP'}")
+        bt.logging.debug(f"{'Metric TSP' if prob_select == 1 else 'General TSP'}")
         bt.logging.debug(f"GraphProblem Validation Error: {e.json()}")
         bt.logging.debug(e.errors())
         bt.logging.debug(e)
@@ -49,13 +50,15 @@ def generate_problem():
 def generate_problem_from_dataset(min_node=2000, max_node=5000):
     n_nodes = random.randint(min_node, max_node)
     # randomly select n_nodes indexes from the selected graph
-    prob_select = random.randint(0, len(list(loaded_datasets.keys()))-1)
+    prob_select = random.randint(0, len(list(loaded_datasets.keys())) - 1)
     dataset_ref = list(loaded_datasets.keys())[prob_select]
     bt.logging.info(f"n_nodes V2 {n_nodes}")
-    bt.logging.info(f"dataset ref {dataset_ref} selected from {list(loaded_datasets.keys())}" )
-    bt.logging.info(f"dataset length {len(loaded_datasets[dataset_ref]['data'])} from {loaded_datasets[dataset_ref]['data'].shape} " )
+    bt.logging.info(f"dataset ref {dataset_ref} selected from {list(loaded_datasets.keys())}")
+    bt.logging.info(
+        f"dataset length {len(loaded_datasets[dataset_ref]['data'])} from {loaded_datasets[dataset_ref]['data'].shape} ")
     selected_node_idxs = random.sample(range(len(loaded_datasets[dataset_ref]['data'])), n_nodes)
-    test_problem_obj = GraphV2Problem(problem_type="Metric TSP", n_nodes=n_nodes, selected_ids=selected_node_idxs, cost_function="Geom", dataset_ref=dataset_ref)
+    test_problem_obj = GraphV2Problem(problem_type="Metric TSP", n_nodes=n_nodes, selected_ids=selected_node_idxs,
+                                      cost_function="Geom", dataset_ref=dataset_ref)
 
     try:
         graphsynapse_req = GraphV2Synapse(problem=test_problem_obj)
@@ -67,19 +70,21 @@ def generate_problem_from_dataset(min_node=2000, max_node=5000):
     return graphsynapse_req
 
 
-def generate_problem_for_mTSP(min_node=500, max_node=2000,min_salesman=2, max_salesman=10):
+def generate_problem_for_mTSP(min_node=500, max_node=2000, min_salesman=2, max_salesman=10):
     n_nodes = random.randint(min_node, max_node)
-    prob_select = random.randint(0, len(list(loaded_datasets.keys()))-1)
+    prob_select = random.randint(0, len(list(loaded_datasets.keys())) - 1)
     dataset_ref = list(loaded_datasets.keys())[prob_select]
     bt.logging.info(f"n_nodes V2 {n_nodes}")
-    bt.logging.info(f"dataset ref {dataset_ref} selected from {list(loaded_datasets.keys())}" )
+    bt.logging.info(f"dataset ref {dataset_ref} selected from {list(loaded_datasets.keys())}")
     selected_node_idxs = random.sample(range(len(loaded_datasets[dataset_ref]['data'])), n_nodes)
     m = random.randint(min_salesman, max_salesman)
-    test_problem_obj = GraphV2ProblemMulti(problem_type="Metric mTSP", n_nodes=n_nodes, selected_ids=selected_node_idxs, cost_function="Geom", dataset_ref=dataset_ref, n_salesmen=m, depots=[0]*m)
+    test_problem_obj = GraphV2ProblemMulti(problem_type="Metric mTSP", n_nodes=n_nodes, selected_ids=selected_node_idxs,
+                                           cost_function="Geom", dataset_ref=dataset_ref, n_salesmen=m, depots=[0] * m)
     try:
 
         graphsynapse_req = GraphV2Synapse(problem=test_problem_obj)
-        bt.logging.info(f"GraphV2Synapse {graphsynapse_req.problem.problem_type}, n_nodes: {graphsynapse_req.problem.n_nodes}")
+        bt.logging.info(
+            f"GraphV2Synapse {graphsynapse_req.problem.problem_type}, n_nodes: {graphsynapse_req.problem.n_nodes}")
     except ValidationError as e:
         bt.logging.debug(f"GraphV2Synapse Validation Error: {e.json()}")
         bt.logging.debug(e.errors())
@@ -87,26 +92,36 @@ def generate_problem_for_mTSP(min_node=500, max_node=2000,min_salesman=2, max_sa
 
     return graphsynapse_req
 
-def mTSP_solve(min_node, max_node,min_salesman=2,max_salesman=3):
-    synapse = generate_problem_for_mTSP(min_node=min_node, max_node=max_node, min_salesman=min_salesman, max_salesman=max_salesman)
+
+def mTSP_solve(min_node, max_node, min_salesman=2, max_salesman=3):
+    synapse = generate_problem_for_mTSP(min_node=min_node, max_node=max_node, min_salesman=min_salesman,
+                                        max_salesman=max_salesman)
     print(f'synapse = {synapse}')
 
-    edges = recreate_edges(synapse.problem,factor=1).tolist()
+    edges = recreate_edges(synapse.problem, factor=1).tolist()
     synapse.problem.edges = edges
 
     print(f'start running or-solver')
+    t0 = time.time()
     nn_multi_synapse = asyncio.run(nn_multi_solver_solution(synapse))
+    t1 = time.time()
     print(f'nn_multi_synapse = {nn_multi_synapse}')
+    print(f"time baseline  = {t1 - t0}, num node = {synapse_request.problem.n_nodes}")
+
+    list_synapse = [nn_multi_synapse]
+    scores = [scoring_solution(synapse) for synapse in list_synapse]
+    min_score = min(scores)
+    scores.append(min_score)
+    print(f'scores = {scores}')
 
 
-
-def compare(gen_func=None, min_node = 2000, max_node = 5000):
+def compare(gen_func=None, min_node=2000, max_node=5000):
     if gen_func == 'V2':
         synapse_request = generate_problem_from_dataset(min_node=min_node, max_node=max_node)
         start = time.time()
         edges = recreate_edges(synapse_request.problem).tolist()
         end = time.time()
-        print(f'time calculating edges: {end-start}')
+        print(f'time calculating edges: {end - start}')
         synapse_request.problem.edges = edges
         # body_dict = {
         #     'route':edges
@@ -126,16 +141,16 @@ def compare(gen_func=None, min_node = 2000, max_node = 5000):
     print(f"lkh_input_file = {lkh_input_file}")
     # beam_synapse = asyncio.run(beam_solver_solution(synapse_request))
     t2 = time.time()
-    print(f'time build input file: {t2-t1}')
+    print(f'time build input file: {t2 - t1}')
     baseline_synapse = asyncio.run(baseline_solution(synapse_request))
     t3 = time.time()
     # nns_vali_synapse = asyncio.run(nns_vali_solver_solution(synapse_request))
     t4 = time.time()
-    lkh_synapse = asyncio.run(lkh_solver_solution(synapse_request,input_file=lkh_input_file))
+    lkh_synapse = asyncio.run(lkh_solver_solution(synapse_request, input_file=lkh_input_file))
     t5 = time.time()
     # new_synapse = asyncio.run(new_solver_solution(synapse_request))
     t6 = time.time()
-    lkh_synapse_3 = asyncio.run(lkh_solver_solution(synapse_request, num_run=3,input_file=lkh_input_file))
+    lkh_synapse_3 = asyncio.run(lkh_solver_solution(synapse_request, num_run=3, input_file=lkh_input_file))
     t7 = time.time()
 
     # time_point = [t1, t2, t3, t4, t5, t6, t7]
@@ -148,8 +163,9 @@ def compare(gen_func=None, min_node = 2000, max_node = 5000):
     #         print(f'time process of {i} > 20')
     #         exit(0)
 
-    print(f"time baseline = {t3-t2}, time lkh = {t5-t4}, time lkh3 = {t7 - t6} ,num node = {synapse_request.problem.n_nodes}")
-    list_synapse = [baseline_synapse,lkh_synapse, lkh_synapse_3]
+    print(
+        f"time baseline = {t3 - t2}, time lkh = {t5 - t4}, time lkh3 = {t7 - t6} ,num node = {synapse_request.problem.n_nodes}")
+    list_synapse = [baseline_synapse, lkh_synapse, lkh_synapse_3]
     scores = [scoring_solution(synapse) for synapse in list_synapse]
     # scores = [1e+20, 1e+20, 1e+20]
     #
@@ -175,17 +191,19 @@ def compare(gen_func=None, min_node = 2000, max_node = 5000):
     print(f'score = {scores}')
     return scores
 
-def recreate_edges(problem: Union[GraphV2Problem, GraphV2ProblemMulti],factor=1):
+
+def recreate_edges(problem: Union[GraphV2Problem, GraphV2ProblemMulti], factor=1):
     node_coords_np = loaded_datasets[problem.dataset_ref]["data"]
     node_coords = np.array([node_coords_np[i][1:] for i in problem.selected_ids])
     if problem.cost_function == "Geom":
-        return geom_edges(node_coords,factor=factor)
+        return geom_edges(node_coords, factor=factor)
     elif problem.cost_function == "Euclidean2D":
         return euc_2d_edges(node_coords)
     elif problem.cost_function == "Manhatten2D":
         return man_2d_edges(node_coords)
     else:
         return "Only Geom, Euclidean2D, and Manhatten2D supported for now."
+
 
 def review_solution_new_meta():
     synapse_request = generate_problem_from_dataset(min_node=200, max_node=500)
@@ -196,7 +214,7 @@ def review_solution_new_meta():
     synapse_request.problem.edges = edges
     synapse = asyncio.run(hpn_solver_solution(synapse_request))
     t2 = time.time_ns()
-    print(f'time processing: {(t2-t1)/1e6} ms')
+    print(f'time processing: {(t2 - t1) / 1e6} ms')
     score = scoring_solution(synapse)
     print(f'score = {score}')
 
@@ -211,8 +229,8 @@ def calculate_raw_data(config_file):
     problem_type = config['problem_type']['value']
     n_nodes = config['n_nodes']['value']
     test_graph_problem = GraphV2Problem(problem_type=problem_type, objective_function=objective_function,
-                                     n_nodes=n_nodes, selected_ids=selected_ids,
-                                     cost_function="Geom", dataset_ref=dataset_ref)
+                                        n_nodes=n_nodes, selected_ids=selected_ids,
+                                        cost_function="Geom", dataset_ref=dataset_ref)
 
     synapse_request = GraphV2Synapse(problem=test_graph_problem)
     print(f'synapse_request = {synapse_request}')
@@ -263,8 +281,6 @@ if __name__ == '__main__':
     # score = scoring_solution(synapse)
     # print(f'score = {score}')
 
-
-
     # synapse_request = generate_problem()
     # # print(f"synapse_request = {synapse_request}")
     # json_data = json.dumps(synapse_request.problem.dict())
@@ -300,4 +316,3 @@ if __name__ == '__main__':
     # print(f'time processing convert edge {(end_time - start_time)/1e6} ms with number of node: {synapse_req.problem.n_nodes}')
     # # print(f'edges = {edges}')
     # time.sleep(0.2)
-
