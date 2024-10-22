ea7acd0b08e40de9e4fed3066478d42db25222ae
dungnguyen <dung.kstncnttk56@gmail.com>
2024-10-22 22:53:40 +0700

    scale lkh matrix

--- neurons/test.py
+++ neurons/test.py
@@ -36,7 +36,7 @@ def check_call_api():
     print(f'time processing total : {end-start}')
 
 def check_mtsp():
-    mTSP_solve(min_node=2000, max_node=2000, min_salesman=10, max_salesman=10, dataset_ref='World_TSP')
+    mTSP_solve(min_node=2000, max_node=2000, min_salesman=2, max_salesman=2, dataset_ref='World_TSP')
 
 
 if __name__ == '__main__':
