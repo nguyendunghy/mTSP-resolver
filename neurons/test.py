472bc1beab9eb2ffbab91a2cf26a52b72eae5a4b
dungnguyen <dung.kstncnttk56@gmail.com>
2024-10-23 23:27:39 +0700

    setup objective

--- neurons/test.py
+++ neurons/test.py
@@ -36,7 +36,7 @@ def check_call_api():
     print(f'time processing total : {end-start}')
 
 def check_mtsp():
-    mTSP_solve(min_node=500, max_node=500, min_salesman=4, max_salesman=4, dataset_ref='World_TSP')
+    mTSP_solve(min_node=500, max_node=500, min_salesman=10, max_salesman=10, dataset_ref='World_TSP')
 
 
 if __name__ == '__main__':
