1d00f656229e10d3ec31642bbe57e44213bdcc68
dungnguyen <dung.kstncnttk56@gmail.com>
2024-10-24 15:20:00 +0700

    prepare for freelancers

--- neurons/test.py
+++ neurons/test.py
@@ -5,7 +5,7 @@ from neurons.compare_solutions import solve_mTSP
 
 
 def check_mTSP():
-    data = [solve_mTSP(min_node=31, max_node=35, min_salesman=2, max_salesman=10) for i in tqdm.tqdm(range(20))]
+    data = [solve_mTSP(min_node=500, max_node=200, min_salesman=2, max_salesman=10) for i in tqdm.tqdm(range(20))]
     data = np.array(data)
 
     print("BASELINE:", data[:, 0].mean())
